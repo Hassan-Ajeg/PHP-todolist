2d2e9db38b0a423d0d32d4a14aa2ad83b5f2b51a
Hassan-92 <hassan.ajeg@hotmail.fr>
2020-04-10 11:52:40 +0200

    création de la connexion à la BD avec une instance de PDO

--- web/index.php
+++ web/index.php
@@ -10,5 +10,18 @@ if(! file_exists($controllerPath)) {
     $errorMessage = "Fichier introuvable !";
     $controllerPath = "../controllers/errorController.php";
 }
+
+//ouverture de la connexion à la BD
+$pdo = new PDO(
+    "mysql:host=127.0.0.1;dbname=todolist;charset=utf8",
+    "root",
+    "root",
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
+
+
 //execution du contrôleur 
 require $controllerPath;
